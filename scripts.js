98e4d6dff5b4a70faa0b78da6d762b0ce7d48806
Gilmar Tambot <151186870+santiagosegundo@users.noreply.github.com>
2023-11-24 16:41:10 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
+//fecha automatica
+const fechaInput = document.getElementById("fecha");
+const meses = [
+    "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"
+  ];
+const fechaActual = new Date();
+const dia = fechaActual.getDate().toString().padStart(2, "0");
+const mes = meses[fechaActual.getMonth()];
+const anio = fechaActual.getFullYear();
+
+const fechaFormateada = `${dia}/${mes}/${anio}`;
+fechaInput.value = fechaFormateada;
+
 function enviarDatos() {
     var nombre = document.getElementById("nombre").value;
     var edad = document.getElementById("edad").value;
-  
     fetch('https://script.google.com/macros/s/AKfycbwspEBvrOMb5Qbm0qCqr-UpZ7nlBiDte0P8lcvUk93hxRTV1B9qOOCQlJvvKD327YDt/exec', {
       method: 'post',
       mode: 'cors',
